dccb693088c904836bf757ac6e312cd124dcbe03
chiguozi <qdlzr456@163.com>
2019-03-09 18:14:08 +0800

    log

--- assets/scripts/core/debug/Logger.ts
+++ assets/scripts/core/debug/Logger.ts
@@ -2,6 +2,16 @@ export default class Logger
 {
     public log(content)
     {
-        
+        cc.log(content);
+    }
+
+    public warn(content)
+    {
+        cc.warn(content);
+    }
+
+    public error(content)
+    {
+        cc.error(content);
     }
 }
